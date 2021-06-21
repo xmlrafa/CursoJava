4621defc528be6aaa29c36c8a880078f3d078d08
Rafael Rodrigues <81927036+xmlrafa@users.noreply.github.com>
2021-06-21 15:15:22 -0300

    exercicios

---
+++ exercicios/src/oo/encapsulamento/Pessoa.java
@@ -0,0 +1,40 @@
+package oo.encapsulamento;
+
+public class Pessoa {
+	private String nome;
+	private int idade;
+	
+	public Pessoa(String nome, int idade) {
+	setIdade(idade);
+	setNome(nome);
+	}
+	
+	
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	// Getter
+	public int getIdade() {
+		return idade;
+	}
+	
+	public String toString() {
+		return "Olá eu sou o " + getNome() + " e tenho "
+				+ getIdade() + " anos.";
+	}
+	
+	public void setIdade(int novaIdade) {
+		novaIdade=Math.abs(novaIdade);
+		if (novaIdade >= 0 && novaIdade <=130) {
+			this.idade = novaIdade;	
+		}
+		
+	}
+}
---
+++ exercicios/src/oo/encapsulamento/PessoaTeste.java
@@ -0,0 +1,12 @@
+package oo.encapsulamento;
+
+public class PessoaTeste {
+	public static void main(String[] args) {
+	Pessoa p1 = new Pessoa("Rafa",1);
+	// p1.idade = -30;
+	p1.setIdade(-440);
+	
+	System.out.println(p1);
+	}
+	
+}
